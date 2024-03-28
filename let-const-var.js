d66f2e2ec55cb8fad18442b417c7d94b27932302
Lsnnov1 <154484671+Lsnnov1@users.noreply.github.com>
2024-03-28 13:45:20 -0400

    init

---
+++ let-const-var.js
@@ -0,0 +1,20 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+/* Write an ES2015 Version */
+
+const PI = 3.14
+
+
+// - What is the difference between var and let?
+// VAR HAS A FUNCTION SCOPE AND CAN BE REDECLARED, LET HAS A BLOCK SCOPE AND CANNOT BE REDECLARED
+
+// - What is the difference between var and const?
+// VAR HAS A FUNCTION SCOPE, CAN BE REDECLARED AND REASSIGNED, CONST CANNOT BE REDECLARED, REASSIGNED, AND HAS A BLOCK SCOPE
+
+// - What is the difference between let and const?
+// LET CAN BE REASSIGNED, WHILE CONST CANNOT.
+
+// - What is hoisting?
+// CAN ONLY BE USED BY VAR. GIVES ACCESS TO VARIABLE BEFORE VARIABLE IS EVEN ASSIGNED TO A VALUE.
